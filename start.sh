3f416834a8b97c50f4e017659e23f58a0d430b15
Mao Moon <maoandmoon@gmail.com>
2018-11-11 16:26:18 +0300

    add media

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 virtualenv venv --python=python3
-. venv/bin/activate
+source venv/bin/activate
+venv/bin/python -m pip install -r requirements.txt
 npm install
-python -m pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py makemigrations salon
 python manage.py migrate
