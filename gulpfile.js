2fed6ea7e7f229a6f777d23daed4a52dafdd5df3
Mao Moon <maoandmoon@gmail.com>
2018-11-17 08:34:43 +0300

    bug fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,25 @@
 const gulp = require('gulp');
 const watch = require('gulp-watch');
 const livereload = require('gulp-livereload');
+const imagemin = require('gulp-imagemin');
+const autoprefixer = require('gulp-autoprefixer');
 const cssmin = require('gulp-cssmin');
 const rename = require('gulp-rename');
 const minify = require('gulp-minify');
+
 const spawn = require('child_process').spawn;
 const open = require('gulp-open');
-// const imagemin = require('gulp-imagemin');
-// const autoprefixer = require('gulp-autoprefixer');
 
+
+const gzip_options = {
+    threshold: '1kb',
+    gzipOptions: {
+        level: 9
+    }
+};
 
 gulp.task('css-minify', function() {
-    gulp.src('./salon/static/css/style.css')
+    gulp.src(['./salon/static/css/*.css', '!./salon/static/css/*.min.css'])
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('./salon/static/css'))
@@ -21,6 +29,11 @@ gulp.task('js-minify', function() {
     gulp.src('./salon/static/js/main.js')
         .pipe(minify({ext:{min:'.min.js'}, noSource: true,}))
         .pipe(gulp.dest('./salon/static/js'))
+});
+
+gulp.task('uri', function(){
+  gulp.src(__filename)
+  .pipe(open({uri: 'http://192.168.0.105:8000/'}));
 });
 
 
@@ -41,14 +54,9 @@ gulp.task('django', function() {
 
 gulp.task('go', function() {
     livereload.listen();
-    watch("salon/static/css/style.css", ["css-minify"]);
-    watch("salon/static/js/main.js", ['js-minify']);
-    watch(["salon/static/js/main.js", "salon/templates/*", "salon/static/css/style.css"]).on('change', livereload.changed);
-});
-
-gulp.task('uri', function(){
-  gulp.src(__filename)
-  .pipe(open({uri: 'http://192.168.0.105:8000/'}));
+    gulp.watch(["salon/static/css/*.css","!salon/static/css/*.min.css" ], ["css-minify"]);
+    gulp.watch("salon/static/js/main.js", ['js-minify']);
+    gulp.watch(["salon/static/js/*", "salon/templates/*", "salon/static/css/*"]).on('change', livereload.changed);
 });
 
 gulp.task('default', ['django', 'go', 'uri']);
